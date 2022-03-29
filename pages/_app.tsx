6d6cfd2a83c3046366fede278eac4b4ba82e7155
Abyss <abyss002@pm.me>
2022-03-29 19:18:24 +0100

    Update to initial design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,32 @@
-import type { AppProps } from 'next/app'
+import AppProps from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+    const { Component, pageProps } = props;
+
+    return (
+        <>
+            <Head>
+                <title>Abyss</title>
+                <meta name="description" content="Portfolio for Abyss" />
+                <link rel="icon" href="/me.png" />
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{
+                    /** Put your mantine theme override here */
+                    colorScheme: "dark",
+                }}
+            >
+                <Component {...pageProps} />
+            </MantineProvider>
+        </>
+    );
 }
-
-export default MyApp
